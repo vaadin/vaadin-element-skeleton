558c7215382290b52101fd76810f1066065393cf
Limon Monte <limon.monte@gmail.com>
2017-01-17 16:16:32 +0200

    Remove Linux/android@5.1 from automated SauceLabs tests

--- wct.conf.js
+++ wct.conf.js
@@ -1,16 +1,13 @@
 module.exports = {
   registerHooks: function(context) {
-    // The Good
     var crossPlatforms = [
       'Windows 10/chrome@55',
       'Windows 10/firefox@50'
     ];
 
-    // The Bad
     var otherPlatforms = [
       'OS X 10.11/iphone@9.3',
       'OS X 10.11/ipad@9.3',
-      'Linux/android@5.1',
       'Windows 10/microsoftedge@13',
       'Windows 10/internet explorer@11',
       'OS X 10.11/safari@10.0'
