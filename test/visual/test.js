ed47b0a37fd54b60822c02f9e979cd14a7f3db65
Limon Monte <limon.monte@gmail.com>
2017-04-29 12:11:17 +0300

    Bump lighthouse plugin (#35)
    * Bump lighthouse plugin, increase threshold to 0.85
    
    * Fix Firefox gemini tests sukablya

--- test/visual/test.js
+++ test/visual/test.js
@@ -1,4 +1,18 @@
 gemini.suite('vaadin-element', function(rootSuite) {
+  function wait(actions, find) {
+    actions.wait(5000);
+  }
+
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
 
   gemini.suite('default-tests', function(suite) {
     suite
@@ -7,9 +21,7 @@ gemini.suite('vaadin-element', function(rootSuite) {
       .before(function(actions, find) {
         this.button = find('button');
       })
-      .capture('normal-button', {}, function(actions, find) {
-        actions.wait(3000);
-      })
+      .capture('normal-button')
       .capture('clicked-button', function(actions) {
         actions.mouseDown(this.button);
       });
