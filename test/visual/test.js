32d84cb11725981b8a6173f0d8689d3baf6948cd
web-padawan <serguey.kulikov@gmail.com>
2019-06-03 14:05:03 +0300

    Restore visual tests using gemini in SauceLabs

---
+++ test/visual/test.js
@@ -0,0 +1,18 @@
+gemini.suite('vaadin-element', function(rootSuite) {
+  function wait(actions, find) {
+    return actions.wait(5000);
+  }
+
+  rootSuite
+    .before(wait);
+
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`default-tests-${theme}`, function(suite) {
+      suite
+        .setUrl(`default.html?theme=${theme}`)
+        .setCaptureElements('#default-tests')
+        .capture(`vaadin-element`);
+    });
+  });
+
+});
